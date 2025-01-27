07e2e72786f059c2ee7da5bd76e0507665f30f52
kaesik <108717713+kaesik@users.noreply.github.com>
2025-01-27 23:35:43 +0100

    common things

---
+++ shared/src/commonMain/kotlin/com/kaesik/tabletopwarhammer/core/domain/util/CommonStateFlow.kt
@@ -0,0 +1,8 @@
+package com.kaesik.tabletopwarhammer.core.domain.util
+
+import kotlinx.coroutines.flow.StateFlow
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+expect class CommonStateFlow<T>(flow: StateFlow<T>): StateFlow<T>
+
+fun <T> StateFlow<T>.toCommonStateFlow() = CommonStateFlow(this)
---
+++ shared/src/commonMain/kotlin/com/kaesik/tabletopwarhammer/core/presentation/Colors.kt
@@ -0,0 +1,10 @@
+package com.kaesik.tabletopwarhammer.core.presentation
+
+object Colors {
+    const val ACCENT_VIOLET = 0xFF5643C9
+    const val LIGHT_BLUE = 0xFFA8A5BB
+    const val LIGHT_BLUE_GREY = 0xFFF6F4F4
+    const val DARK_GREY = 0xFF282C31
+    const val LIGHT_GREY = 0xFFEEEEEE
+    const val TEXT_BLACK = 0xFF111111
+}
