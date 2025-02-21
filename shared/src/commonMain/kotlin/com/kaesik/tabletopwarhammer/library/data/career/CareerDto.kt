499f43943f1da3dda99d3da430c5e0ecc8e7f345
kaesik <108717713+kaesik@users.noreply.github.com>
2025-02-21 12:07:09 +0100

    working on data base

--- shared/src/commonMain/kotlin/com/kaesik/tabletopwarhammer/library/data/career/CareerDto.kt
+++ shared/src/commonMain/kotlin/com/kaesik/tabletopwarhammer/library/data/career/CareerDto.kt
@@ -1,12 +1,33 @@
 package com.kaesik.tabletopwarhammer.library.data.career
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class CareerDto(
+    @SerialName("id")
     val id: String,
+    @SerialName("name")
     val name: String,
+    @SerialName("limitations")
+    val limitations: String,
+    @SerialName("summary")
+    val summary: String,
+    @SerialName("description")
     val description: String,
-    val shortName: String,
+    @SerialName("advance_scheme")
+    val advanceScheme: String,
+    @SerialName("quotations")
+    val quotations: String,
+    @SerialName("adventuring")
+    val adventuring: String,
+    @SerialName("source")
+    val source: String,
+    @SerialName("career_path")
+    val careerPath: String,
+    @SerialName("class_name")
+    val className: String,
+    @SerialName("page")
     val page: Int,
-)
+
+    )
