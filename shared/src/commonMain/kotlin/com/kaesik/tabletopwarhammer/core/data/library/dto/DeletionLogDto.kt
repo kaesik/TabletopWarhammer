34b1cd9072c5dddbac686d38945d83a5e07ee77e
kaesik <108717713+kaesik@users.noreply.github.com>
2025-11-28 23:33:27 +0100

    working on offline-first datasync

---
+++ shared/src/commonMain/kotlin/com/kaesik/tabletopwarhammer/core/data/library/dto/DeletionLogDto.kt
@@ -0,0 +1,10 @@
+package com.kaesik.tabletopwarhammer.core.data.library.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DeletionLogDto(
+    @SerialName("entity_id") val entityId: String,
+    @SerialName("deleted_at") val deletedAt: String
+)
